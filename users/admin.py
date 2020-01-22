8e6e90b3a4a980d740f5667b8496a9b2d5e06734
BKLemonTea <jyf1128@naver.com>
2020-01-23 02:39:46 +0900

    Day_3

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
-@admin.register(models.User)
+@admin.register(models.User) #decorator
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+    
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields" : (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            }
+        ),
+    )
+
+"""
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    
+    #Custom User Admin
+    
+    # User페이지에서 Table명을 바꿔주고 해당 옵션을 보여줌.
+    list_display = ("username","email", "gender", "language", "currency", "superhost")
+    
+    # 해당 필드의 필터를 보여줌.
+    list_filter = ("language","currency","superhost",)
+"""
+
+"""
+둘다 같음
+@admin.register(models.User) 클래스 위에 있어야함
+admin.site.register(models.User, CustomUserAdmin)
+"""

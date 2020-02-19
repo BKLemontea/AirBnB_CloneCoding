9b8697fdeea5de42a726d98c721077d87df7eeea
BKLemonTea <jyf1128@naver.com>
2020-02-19 20:59:26 +0900

    get_page vs page, Handling Exceptions

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,20 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 # Create your views here.
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms))
-    return render(request, "rooms/home.html", context={
-        "rooms":rooms
-    })
+    paginator = Paginator(room_list, 10, orphans=5) #orphans은 마지막 페이지의 항목 수가 5개 이하일 경우 그전 페이지에서 보여줌.
+    try:
+        # paginator.get_page()는 에러가 발생하면 마지막 페이지를 보여줌
+        # paginator.page()는 에러가 발생하면 에러 페이지를 보여줌
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", context={
+        "page":rooms
+        })
+    except EmptyPage:
+        return redirect("/")
+    

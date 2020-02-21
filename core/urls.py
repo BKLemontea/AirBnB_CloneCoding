c4eeb173cb0d0bc67b9a3492d91895bf91046755
BKLemonTea <jyf1128@naver.com>
2020-02-21 23:03:14 +0900

    Class Based Views

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from rooms import views as room_views
 app_name = "core" # app_name의 이름은 config/urls.py의 namespace의 이름과 같아야 한다.
 
 urlpatterns = [
-    path("", room_views.all_rooms, name="home"),
+    path("", room_views.HomeView.as_view(), name="home"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,21 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
-# Create your views here.
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5) #orphans은 마지막 페이지의 항목 수가 5개 이하일 경우 그전 페이지에서 보여줌.
-    try:
-        # paginator.get_page()는 에러가 발생하면 마지막 페이지를 보여줌
-        # paginator.page()는 에러가 발생하면 에러 페이지를 보여줌
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", context={
-        "page":rooms
-        })
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
     
+    """ HomeView Definition """
+    
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    # page_kwarg = "page"
+    context_object_name = "rooms"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+        

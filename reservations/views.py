1a3c8bdd9581dbc9f42f3d87b426551eae94e1a0
BKLemonTea <jyf1128@naver.com>
2020-04-06 11:24:36 +0900

    Model Managers

--- reservations/views.py
+++ reservations/views.py
@@ -28,4 +28,7 @@ def create(request, room, year, month, day):
         return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
     
 class ReservationDetail(View):
-    pass
+    def get(self, pk):
+        reservation = models.Reservation.objects.get_or_none(pk=pk)
+        if not reservation:
+            return redirect(reverse("core:home"))

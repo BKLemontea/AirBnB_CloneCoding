25331d769668a5c340c23b34367824fe0345e7e4
BKLemonTea <jyf1128@naver.com>
2020-04-10 18:40:31 +0900

    Updating a Reservation

--- reservations/urls.py
+++ reservations/urls.py
@@ -6,4 +6,5 @@ app_name = "reservations"
 urlpatterns = [
     path("create/<int:room>/<int:year>-<int:month>-<int:day>/", views.create, name="create"),
     path("<int:pk>/", views.ReservationDetail.as_view(), name="detail"),
+    path("<int:pk>/<str:verb>/", views.edit_reservation, name="edit"),
 ]
